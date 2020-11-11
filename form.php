31d58cecbce7ea28dad80d369b1ea9174257312d
Yandawson <70661462+yandawson@users.noreply.github.com>
2020-11-11 14:05:36 -0500

    Add files via upload

---
+++ form.php
@@ -0,0 +1,9 @@
+<?php
+
+$name1=$_GET['name1'];
+$name2=$_GET['name2'];
+
+echo 'name 1'.$name1;
+echo '<br>name 2'.$name2;
+
+?>
---
+++ lab-8-http.php
@@ -0,0 +1,15 @@
+<html>
+
+<body>
+
+<center>
+<h1> HML Form method</h>
+
+<form action="">
+
+name 1 <input type="name1" name=""><br>
+name 2 <input type="name2" name=""><br>
+
+<input type="submit" value="submit">
+
+</form>
